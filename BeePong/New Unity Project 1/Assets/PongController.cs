2c7177d4abc996f370412532b601e28c8e53013c
Michael <leichtfried.michael@gmail.com>
2013-04-24 18:22:47 +0200

    Kamerafolding, Mechanics Optimierung

--- BeePong/New Unity Project 1/Assets/PongController.cs
+++ BeePong/New Unity Project 1/Assets/PongController.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class PongController : MonoBehaviour {
-
+	
+	private Vector3 beeOriginVelocity;
+	private bool istVelocityStored = false;
 	// Use this for initialization
 	void Start () {
 		Screen.showCursor = false;
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(Screen.lockCursor);
 		Vector3 pos = new Vector3();
 		pos.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x ;
 		this.transform.position = new Vector3(pos.x,this.transform.position.y,this.transform.position.z);
@@ -21,6 +21,12 @@ public class PongController : MonoBehaviour {
 		float velocity = collision.rigidbody.velocity.magnitude;
 		Vector3 colVelocity = collision.rigidbody.velocity;
 		colVelocity.x = (collision.transform.position.x -transform.position.x);
+		if(!istVelocityStored) {
+			beeOriginVelocity = GameObject.Find("Sphere").transform.rigidbody.velocity;
+			istVelocityStored = true;
+		}
+		colVelocity.y = beeOriginVelocity.y * 0.3f;
+		Debug.Log(colVelocity);
 		collision.rigidbody.velocity = colVelocity;
 		if (collision.rigidbody.velocity.magnitude < velocity) {
 			collision.rigidbody.velocity *= velocity/collision.rigidbody.velocity.magnitude;
