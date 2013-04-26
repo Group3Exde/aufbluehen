2a983ef604b0417cde7d239a933a8f976d4c48af
Group3Exde <guppe3exde>
2013-04-26 19:40:32 +0200

    der letzte versuch

---
+++ BeePong/New Unity Project 1/Assets/FloorCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class FloorCollision : MonoBehaviour {
+	bool colide = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Sphere"){
+		colide = true;
+		}
+	}
+	void OnGUI(){
+		if(colide){
+		GUI.Box(new Rect(Screen.width/2, Screen.height/2,100,50), "GAME OVER");
+		GameObject.Destroy(GameObject.Find("Sphere"));
+		}
+	}
+}
